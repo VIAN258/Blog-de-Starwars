dd9549d5d50bdd5a15c875d8161e95e37289dd7f
VIAN258 <angelmorales891@gmail.com>
2023-02-17 17:20:33 +0000

    salvando cambios1

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,23 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			
+		<nav className="navbar bg-black px-5 py-4">
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Favoritos</button>
-				</Link>
+			    <img className="navbar-brand container-fluid"src="https://upload.wikimedia.org/wikipedia/commons/2/21/Star_Wars_logo.png" alt="" width="30" height="60"></img>
+			</div>
+			<div className="ml-auto">
+				<div class="btn-group">
+					<button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+						Favoritos
+					</button>
+					<ul class="dropdown-menu dropdown-menu-dark">	
+					</ul>
+		        </div>
 			</div>
 		</nav>
 	);
 };
+
+
+
+
